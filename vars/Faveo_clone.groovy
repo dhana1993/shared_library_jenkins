9976e9b9373979d0995b0850116e48b55b56b1ee
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-15 16:14:09 +0530

    Update Faveo_clone.groovy

--- vars/Faveo_clone.groovy
+++ vars/Faveo_clone.groovy
@@ -1,4 +1,4 @@
 def call(){
-    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${BRANCH_NAME}"]], extensions: [], userRemoteConfigs: [[credentialsId: '', url: "${GIT_URL}"]]]
+    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${BRANCH_NAME}"]], extensions: [], userRemoteConfigs: [[credentialsId: 'globalservices', url: "${GIT_URL}"]]]
 
 }
