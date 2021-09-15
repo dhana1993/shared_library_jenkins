17a3fc39a1a3ae43e4bd897120d9a6cf80697166
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-15 15:46:53 +0530

    Update cred_get.groovy

--- vars/cred_get.groovy
+++ vars/cred_get.groovy
@@ -1,3 +1,6 @@
-def call(Map config = [:]) {
-    sh "echo NAME: ${config.name}  . PASSWORD: ${config.password}."
+//def call(Map config = [:]) {
+  //  sh "echo NAME: ${config.name}  . PASSWORD: ${config.password}."
+//}
+def call(){
+    sh 'echo NAME: ${EXAMPLE_CREDS_USR}  . PASSWORD:${EXAMPLE_CREDS_PSW}.'
 }
