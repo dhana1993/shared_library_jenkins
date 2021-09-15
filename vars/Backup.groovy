c40858b142483590909681d54807fc39077e6513
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-15 14:00:47 +0530

    Update Backup.groovy

--- vars/Backup.groovy
+++ vars/Backup.groovy
@@ -1,3 +1,3 @@
 def call() {
-     sh ''' sudo mkdir $(date +"%d-%m-%Y")-$BUILD_NUMBER && cp dist/NewFaveo.war $(date +"%d-%m-%Y")-$BUILD_NUMBER  '''
+     sh ''' sudo mkdir $(date +"%d-%m-%Y")-$BUILD_NUMBER   '''
 }
