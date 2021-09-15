c68eb2db2866319b5a9131bdf583f23575dfc6cc
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-15 16:50:37 +0530

    Update mailfailure.groovy

--- vars/mailfailure.groovy
+++ vars/mailfailure.groovy
@@ -1,5 +1,5 @@
 def call(){
-    emailext body:'${SCRIPT,template="groovy-html.template"}\n SonarQube result URL:  "${URL}" ',
+    emailext body:'${SCRIPT,template="groovy-html.template"}\n SonarQube result URL:  "${MY_URL}" ',
                     mimeType: 'text/html',
                     to: "${EMAIL_TO}",
                     subject: 'Jenkins build information : $PROJECT_NAME - #$BUILD_NUMBER '
