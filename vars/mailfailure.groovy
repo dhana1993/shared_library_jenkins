426e3de2dc1eadf6024337c0ec65523f74c064fe
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-15 16:03:32 +0530

    Update mailfailure.groovy

--- vars/mailfailure.groovy
+++ vars/mailfailure.groovy
@@ -1,5 +1,5 @@
-def call(Map config = [:]){
-    emailext body: "Hi team   \n Build is failed in  ${config.FAILED_STAGE} \n BUILD URL : ${BUILD_URL}  \n ",
-            to: "${config.EMAIL_TO}",
+def call(){
+    emailext body: "Hi team   \n Build is failed in  ${FAILED_STAGE} \n BUILD URL : ${BUILD_URL}  \n ",
+            to: "${EMAIL_TO}",
             subject: 'Jenkins build information : $PROJECT_NAME - #$BUILD_NUMBER '
 }
