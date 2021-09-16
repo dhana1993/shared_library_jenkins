c4a01199f86d9e4e5e01a026a3861e98b45a22b1
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-16 14:05:29 +0530

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -2,12 +2,12 @@ def call(){
     def DIR= libraryResource "com"
     sh''' 
     
-   if [ -d "$DIR" ]; then
+   if [ -d "com" ]; then
   ### Take action if $DIR exists ###
   echo "Installing config files in ${DIR}..."
 else
   
-  cp libraryResource "com" .
+  cp ${DIR} .
   exit 1
 fi
     '''
