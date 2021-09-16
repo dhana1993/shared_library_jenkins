4c0700123fb4d81b0d27e4d35720148fb20cae3c
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-16 12:37:51 +0530

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,13 +1,14 @@
 def call(){
     def request = libraryResource 'com'
-   
+    sh '''
     if [ -d "${request}" ]; then
   ### Take action if $DIR exists ###
   echo "Installing config files in ccccccccccoooooommm "${request}"..."
 else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "Error: COCMMMMMMMMMM not found. Can not continue."
-    sh '''cp "${request}" .'''
+   cp "${request}" .
   exit 1
 fi
+'''
 }
