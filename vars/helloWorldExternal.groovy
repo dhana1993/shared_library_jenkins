86424e5bc45356914baaff63b26a243254fff373
dhana1993 <54979357+dhana1993@users.noreply.github.com>
2021-09-16 13:39:49 +0530

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,5 +1,13 @@
 def call(){
-    def path = "resurces/com"
-    sh" echo $path"
-    
+    sh' 
+    def DIR="resurces/com"
+   if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Installing config files in ${DIR}..."
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Error: ${DIR} not found. Can not continue."
+  exit 1
+fi
+    '
 }
